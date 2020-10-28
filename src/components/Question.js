809c8bd998473c7ae900ab9588a63257ab5cf155
DESKTOP-VPN8PF2\User <czycookie@gmail.com>
2020-10-28 18:30:09 +0800

    no message

--- src/components/Question.js
+++ src/components/Question.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './Navbar.css';
 import './Question.css';
 import Collapsible from 'react-collapsible';
+import PaddingContainer from './utilities/PaddingContainer';
+import { Box,Text } from "theme-ui";
 
 
 
@@ -10,57 +12,57 @@ function Question() {
     
 
     return (
-        <div className='padding-container font-family'>
-            <div className='question-header'>
-                <p><strong>Frequently Asked Questions</strong></p>
-            </div>
+        <PaddingContainer>
+            <Text sx={{ fontSize: [24, 32, 40], textAlign: "center", paddingBottom:["2%","2%","3%"] }}>
+                <strong>Frequently Asked Questions</strong>
+            </Text>
 
-            <div className='question-container'>
+            <Box sx={{padding:["0 3%","0 6%","0 8%"]}}>
                     
             <Collapsible open={true} className='openclose-container' trigger="1. What is JomGift?" ><i className="fas fa-minus" style={{float:"right"}}></i>
-                        <div className='content-display'>
-                            <p>fef effbgv hiohiugipu gygypigyu. yftufuofuyfdfg 
+                        <Text sx={{fontSize:[18,24,28], color:"rgb(146,146,146)", paddingTop:["1%",null,"2%"], borderBottom:"1px solid rgb(196,196,196)", paddingBottom:["2%"]}}>
+                            fef effbgv hiohiugipu gygypigyu. yftufuofuyfdfg 
                             gyufufof f67ogdawh, yugeyocgciyg gyugyuogcaig 
-                            huifwehifugwp.</p>
-                        </div>
+                            huifwehifugwp.
+                        </Text>
                     </Collapsible>
                 
                     <Collapsible className='openclose-container' trigger="2. What is JomGift?" ><i className="fas fa-minus" style={{float:"right"}}></i>
-                        <div className='content'>
-                            <p>fef effbgv hiohiugipu gygypigyu. yftufuofuyfdfg 
+                        <Text sx={{fontSize:[18,24,28], color:"rgb(146,146,146)", paddingTop:["1%",null,"2%"], borderBottom:"1px solid rgb(196,196,196)", paddingBottom:["2%"]}}>
+                            fef effbgv hiohiugipu gygypigyu. yftufuofuyfdfg 
                             gyufufof f67ogdawh, yugeyocgciyg gyugyuogcaig 
-                            huifwehifugwp.</p>
-                        </div>
+                            huifwehifugwp.
+                        </Text>
                     </Collapsible>
 
                     <Collapsible className='openclose-container' trigger="3. What is JomGift?" ><i className="fas fa-minus" style={{float:"right"}}></i>
-                        <div className='content'>
-                            <p>fef effbgv hiohiugipu gygypigyu. yftufuofuyfdfg 
+                        <Text sx={{fontSize:[18,24,28], color:"rgb(146,146,146)", paddingTop:["1%",null,"2%"], borderBottom:"1px solid rgb(196,196,196)", paddingBottom:["2%"]}}>
+                            fef effbgv hiohiugipu gygypigyu. yftufuofuyfdfg 
                             gyufufof f67ogdawh, yugeyocgciyg gyugyuogcaig 
-                            huifwehifugwp.</p>
-                        </div>
+                            huifwehifugwp.
+                        </Text>
                     </Collapsible>
 
                     <Collapsible className='openclose-container' trigger="4. What is JomGift?" ><i className="fas fa-minus" style={{float:"right"}}></i>
-                        <div className='content'>
-                            <p>fef effbgv hiohiugipu gygypigyu. yftufuofuyfdfg 
+                        <Text sx={{fontSize:[18,24,28], color:"rgb(146,146,146)", paddingTop:["1%",null,"2%"], borderBottom:"1px solid rgb(196,196,196)", paddingBottom:["2%"]}}>
+                            fef effbgv hiohiugipu gygypigyu. yftufuofuyfdfg 
                             gyufufof f67ogdawh, yugeyocgciyg gyugyuogcaig 
-                            huifwehifugwp.</p>
-                        </div>
+                            huifwehifugwp.
+                        </Text>
                     </Collapsible>
 
                     <Collapsible className='openclose-container' trigger="5. What is JomGift?" ><i className="fas fa-minus" style={{float:"right"}}></i>
-                        <div className='content'>
-                            <p>fef effbgv hiohiugipu gygypigyu. yftufuofuyfdfg 
+                        <Text sx={{fontSize:[18,24,28], color:"rgb(146,146,146)", paddingTop:["1%",null,"2%"], borderBottom:"1px solid rgb(196,196,196)", paddingBottom:["2%"]}}>
+                            fef effbgv hiohiugipu gygypigyu. yftufuofuyfdfg 
                             gyufufof f67ogdawh, yugeyocgciyg gyugyuogcaig 
-                            huifwehifugwp.</p>
-                        </div>
+                            huifwehifugwp.
+                        </Text>
                     </Collapsible>
 
-            </div>
+            </Box>
             
 
-        </div>
+        </PaddingContainer>
 
         
         
